be14653b7dde577e5aa600355ebc274de45c907e
RAJ VARDHAN CHAUDHARY <112545110+rajchaudhary99@users.noreply.github.com>
2024-08-19 15:23:05 +0530

    Create Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
